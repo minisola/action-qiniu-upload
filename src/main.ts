5478dbf350d133992e0ab75992a4f3960597fc76
minisola <blackkid1989@gmail.com>
2024-01-03 23:25:29 +0800

    feat: build

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,20 @@
-import * as core from '@actions/core';
-import { genToken } from './token';
-import { upload } from './upload';
-import qiniu from 'qiniu';
-
+import * as core from "@actions/core";
+import { genToken } from "./token";
+import { upload } from "./upload";
+import qiniu from "qiniu";
 
 async function run(): Promise<void> {
   try {
-    const ak = core.getInput('access_key');
-    const sk = core.getInput('secret_key');
-    const bucket = core.getInput('bucket');
-    const sourceDir = core.getInput('source_dir');
-    const destDir = core.getInput('dest_dir');
-    const ignoreSourceMap = core.getInput('ignore_source_map') === 'true';
+    const ak = core.getInput("access_key");
+    const sk = core.getInput("secret_key");
+    const bucket = core.getInput("bucket");
+    const sourceDir = core.getInput("source_dir");
+    const destDir = core.getInput("dest_dir");
+    const ignoreSourceMap = core.getInput("ignore_source_map") === "true";
 
     const token = genToken(bucket, ak, sk);
 
-    const mac = new qiniu.auth.digest.Mac(ak, sk)
-
+    const mac = new qiniu.auth.digest.Mac(ak, sk);
 
     upload(
       bucket,
@@ -25,10 +23,10 @@ async function run(): Promise<void> {
       destDir,
       ignoreSourceMap,
       (file, key) => core.info(`Success: ${file} => [${bucket}]: ${key}`),
-      () => core.info('Done!'),
-      (error) => core.setFailed(error.message),
+      () => core.info("Done!"),
+      (error) => core.setFailed(error.message)
     );
-  } catch (error) {
+  } catch (error: any) {
     core.setFailed(error.message);
   }
 }
--- src/upload.ts
+++ src/upload.ts
@@ -1,22 +1,22 @@
-import qiniu from 'qiniu';
-import path from 'path';
-import glob from 'glob';
-import pAll from 'p-all';
-import pRetry from 'p-retry';
+import qiniu from "qiniu";
+import path from "path";
+import glob from "glob";
+import pAll from "p-all";
+import pRetry from "p-retry";
 
 function normalizePath(input: string): string {
-  return input.replace(/^\//, '');
+  return input.replace(/^\//, "");
 }
 
 export function upload(
-  bucket:string,
-  mac:any,
+  bucket: string,
+  mac: any,
   srcDir: string,
   destDir: string,
   ignoreSourceMap: boolean,
   onProgress: (srcFile: string, destFile: string) => void,
   onComplete: () => void,
-  onFail: (errorInfo: any) => void,
+  onFail: (errorInfo: any) => void
 ): void {
   const baseDir = path.resolve(process.cwd(), srcDir);
   const files = glob.sync(`${baseDir}/**/*`, { nodir: true });
@@ -24,38 +24,39 @@ export function upload(
   const config = new qiniu.conf.Config();
   const uploader = new qiniu.form_up.FormUploader(config);
 
-  const tasks = files.map((file) => {
-    const relativePath = path.relative(baseDir, path.dirname(file));
-    const key = normalizePath(path.join(destDir, relativePath, path.basename(file)));
+  const tasks = files
+    .map((file) => {
+      const relativePath = path.relative(baseDir, path.dirname(file));
+      const key = normalizePath(
+        path.join(destDir, relativePath, path.basename(file))
+      );
 
-    if (ignoreSourceMap && file.endsWith('.map')) return null;
+      if (ignoreSourceMap && file.endsWith(".map")) return null;
 
-    const task = (): Promise<any> => new Promise((resolve, reject) => {
+      const task = (): Promise<any> =>
+        new Promise((resolve, reject) => {
+          const options = {
+            scope: `${bucket}:${key}`,
+          };
+          const putPolicy = new qiniu.rs.PutPolicy(options);
+          const token = putPolicy.uploadToken(mac);
 
+          const putExtra = new qiniu.form_up.PutExtra();
+          uploader.putFile(token, key, file, putExtra, (err, body, info) => {
+            if (err) return reject(new Error(`Upload failed: ${file}`));
 
-      const options = {
-        scope: `${bucket}:${key}`
-      }
-      const putPolicy = new qiniu.rs.PutPolicy(options)
-      const token = putPolicy.uploadToken(mac)
-      
-      const putExtra = new qiniu.form_up.PutExtra();
-      uploader.putFile(token, key, file, putExtra, (err, body, info) => {
-        if (err) return reject(new Error(`Upload failed: ${file}`));
+            if (info.statusCode === 200) {
+              onProgress(file, key);
+              return resolve({ file, to: key });
+            }
 
-        if (info.statusCode === 200) {
-          onProgress(file, key);
-          return resolve({ file, to: key });
-        }
+            reject(new Error(`Upload failed: ${file}`));
+          });
+        });
 
-        reject(new Error(`Upload failed: ${file}`));
-      });
-    });
+      return () => pRetry(task, { retries: 3 });
+    })
+    .filter((item) => !!item) as (() => Promise<any>)[];
 
-    return () => pRetry(task, { retries: 3 });
-  }).filter((item) => !!item) as (() => Promise<any>)[];
-
-  pAll(tasks, { concurrency: 5 })
-    .then(onComplete)
-    .catch(onFail);
+  pAll(tasks, { concurrency: 5 }).then(onComplete).catch(onFail);
 }
